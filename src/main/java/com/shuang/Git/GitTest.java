1310b6e955b1cb9802944e18fcaa283672341635
zs <1372713212@qq.com>
2021-07-26 14:10:24 +0800

    hot-fix2 commit 1

--- src/main/java/com/shuang/Git/GitTest.java
+++ src/main/java/com/shuang/Git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
     System.out.println("hello git lab！");
     System.out.println("hello hot-fix!");
     System.out.println("hello master-test!");
+    System.out.println("hello hot-fix2!");
   }
 }
