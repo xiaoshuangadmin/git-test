0f9cc349b50b56dfed1d5c72fece05264c452abe
zs <1372713212@qq.com>
2021-07-26 09:49:21 +0800

    first commit

---
+++ src/main/java/com/shuang/Git/GitTest.java
@@ -0,0 +1,12 @@
+package com.shuang.Git;
+
+/**
+ * @DATE: 2021/07/26 09:44
+ * @Author: 小爽帅到拖网速
+ */
+public class GitTest {
+
+  public static void main(String[] args) {
+    System.out.println("hello git！");
+  }
+}
